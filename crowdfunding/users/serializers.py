86a926c3ef9e9e9ec9cd142dbd76dd853ae0d4d1
gudetamago <sharakael@yahoo.com>
2025-10-27 00:51:43 +0800

    Added the actual 'update' method...

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -10,7 +10,18 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
         # The ** is related to kwargs
-    
 
+    def update(self, instance, validated_data):
+        # Remove password from validated_data if present, handle it separately
+        password = validated_data.pop('password', None)
 
+        # Update all other fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
 
+        # Handle password separately if provided - however this is not supported
+        # if password:
+        #     instance.set_password(password)
+
+        instance.save()
+        return instance
